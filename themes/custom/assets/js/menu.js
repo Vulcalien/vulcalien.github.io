51a4fc18adf78c928fc66592781f133e38bd4412
Vulcalien <vulcalien@vulcalien.net>
2026-03-10 20:03:40 +0100

    Theme: turn the hamburger icon into an x after opening the menu

--- themes/custom/assets/js/menu.js
+++ themes/custom/assets/js/menu.js
@@ -1,4 +1,7 @@
 function menu_toggle() {
     const menu = document.querySelector('.site-menu');
     menu.classList.toggle('open');
+
+    const button = document.querySelector('button.menu');
+    button.classList.toggle('open');
 }
